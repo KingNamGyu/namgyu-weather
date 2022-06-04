00c8d550a7bd6df72a32d47fda4808dc5280d21e
KimNamGyu <wkfkem@naver.com>
2022-06-04 19:46:54 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import WeatherBox from "./component/WeatherBox";
+import WeatherButton from "./component/WeatherButton";
+import ClipLoader from "react-spinners/ClipLoader";
+import { Container } from "react-bootstrap";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+//1. 앱이 실행되자마자 현재위치기반의 날씨가 보인다.
+//2. 날씨정보에는 도시, 섭씨 화씨 날씨상태
+//3. 5개의 버튼이 있다 (1개는 현재위치, 4개는 다른도시)
+//4. 도시버튼을 클릭할때 마다 도시별 날씨가 나온다
+//5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다
+//6. 데이터를 들고오는 동안 로딩 스피너가 돈다
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [apiError, setAPIError] = useState("");
+  const cities = ["paris", "new york", "tokyo", "seoul"];
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      getWeatherByCurrentLocation(latitude, longitude);
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setWeather(data);
+      setLoading(false);
+    } catch (err) {
+      setAPIError(err.message);
+      setLoading(false);
+    }
+  };
+
+  const getWeatherByCity = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setWeather(data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setAPIError(err.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (city == null) {
+      setLoading(true);
+      getCurrentLocation();
+    } else {
+      setLoading(true);
+      getWeatherByCity();
+    }
+  }, [city]);
+
+  const handleCityChange = (city) => {
+    if (city === "current") {
+      setCity(null);
+    } else {
+      setCity(city);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="vh-100">
+      {loading ? (
+        <div className="w-100 vh-100 d-flex justify-content-center align-items-center">
+          <ClipLoader color="#f86c6b" size={150} loading={loading} />
+        </div>
+      ) : !apiError ? (
+        <div>
+          <WeatherBox weather={weather} />
+
+          <WeatherButton
+            cities={cities}
+            handleCityChange={handleCityChange}
+            selectedCity={city}
+          />
+        </div>
+      ) : (
+        apiError
+      )}
+    </Container>
   );
 }
-
 export default App;
